31036d4973596859d8319b9f8f6990518f7e71d6
mindcitycode <mindcity@proton.me>
2022-09-17 11:51:13 +0200

    comments

--- src/lib/gl/renderer.js
+++ src/lib/gl/renderer.js
@@ -1,7 +1,6 @@
-
 import { TinySprite } from "./sprite.js"
 import { CreateTexture } from "./utils.js"
-import { loadImage} from '../image.js'
+import { loadImage } from '../image.js'
 
 function Tile(texture, frameX, frameY, frameW, frameH) {
     this.width = frameW;
@@ -15,24 +14,27 @@ function Tile(texture, frameX, frameY, frameW, frameH) {
 }
 
 export const Renderer = async (canvas, imagesUrls) => {
- 
-    var renderer = TinySprite(canvas)
-    renderer.bkg(0,0,0)
 
+    // create gl sprite batch renderer
+    var renderer = TinySprite(canvas)
+    renderer.bkg(0, 0, 0)
+
+    // load images
     const images = await Promise.all(imagesUrls.map(loadImage))
     const textures = images.map(image => CreateTexture(renderer.g, image, image.width, image.height))
 
-    const textureFromImageUrl = imageUrl => textures[imagesUrls.indexOf(imageUrl)]
-
     const tiles = []
+    const getTextureFromImageUrl = imageUrl => textures[imagesUrls.indexOf(imageUrl)]
+    
+    // create a tile, return index
     const makeTile = (imageUrl, frameX, frameY, frameW, frameH) => {
-        const num = tiles.length
-        const texture = textureFromImageUrl(imageUrl)
-        const tile = new Tile(texture, frameX, frameY, frameW, frameH)
-        tiles.push(tile)
-        return num
+        tiles.push(
+            new Tile(getTextureFromImageUrl(imageUrl), frameX, frameY, frameW, frameH)
+        )
+        return tiles.length - 1
     }
 
+    // add a tile to rendering batcg
     const putTile = (tileIndex, x, y, a) => {
         const tile = tiles[tileIndex]
         renderer.img(
